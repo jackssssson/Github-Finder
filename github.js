8087cc93e6796ec87f231b37a19eaaff89f0b515
Zhivko Zhelyazkob <jivko_hinev90@abv.bg>
2019-08-30 11:48:45 +0300

    finished project which i created before 15 days

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = '5e7e165d82b48b500eb3';
+    this.client_secret = 'b6bc21667d3b8866ef0e5b55245882883d84fd15';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
